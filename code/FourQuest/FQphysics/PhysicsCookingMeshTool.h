5317c432a98468b1753d933d1042943f749d4a05
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2024-05-07 11:07:52 +0900

    fix. 물리 엔진 컨벡스 메시 생성 오류 수정

--- code/FourQuest/FQphysics/PhysicsCookingMeshTool.h
+++ code/FourQuest/FQphysics/PhysicsCookingMeshTool.h
@@ -22,7 +22,7 @@ namespace fq::physics
 		/// 모델 데이터의 버텍스 정보들을 읽어서 ConvexMesh 데이터를 추출합니다.
 		/// </summary>
 		/// <returns> PxShape에 들어갈 ConvexMesh를 생성하여 반환합니다. </returns>
-		physx::PxConvexMesh* CookingConvexMesh(DirectX::SimpleMath::Vector3* vertices, int vertexSize, int vertexLimit, int polygonLimit);
+		physx::PxConvexMesh* CookingConvexMesh(DirectX::SimpleMath::Vector3* vertices, int vertexSize, int polygonLimit);
 
 		/// <summary>
 		/// 모델 데이터의 버텍스 포인터 값을 읽어서 해당 포인터의 키 값을 가지고 있는 mConvexMeshMap에 ConvexMesh를 찾아 반환합니다.
@@ -37,6 +37,9 @@ namespace fq::physics
 
 	physx::PxConvexMesh* PhysicsCookingMeshTool::FindConvexMesh(DirectX::SimpleMath::Vector3* vertices)
 	{
-		return  mConvexMeshMap.find(vertices)->second;
+		if (mConvexMeshMap.find(vertices) != mConvexMeshMap.end())
+			return  mConvexMeshMap.find(vertices)->second;
+
+		return nullptr;
 	}
 }
