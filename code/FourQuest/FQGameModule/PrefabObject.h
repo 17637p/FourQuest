e712e73e887b7579c9a8ac28a88112b836b052ad
이기태 <107969311+gigiTae@users.noreply.github.com>
2024-05-09 11:03:47 +0900

    Faet : 프리팹 오브젝트 추가

---
+++ code/FourQuest/FQGameModule/PrefabObject.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <string>
+
+namespace fq::game_module
+{
+	/// <summary>
+	/// 프리팹 인스턴스 
+	/// </summary>
+	class PrefabObject
+	{
+		using Path = std::wstring;
+	public:
+		/// <summary>
+		/// 프리팹의 경로를 반환합니다 
+		/// </summary>
+		/// <returns></returns>
+		fq::game_module::PrefabObject::Path GetPrefabPath() const { return mPrefabPath; }
+
+		/// <summary>
+		/// 프리팹의 경로를 설정합니다
+		/// </summary>
+		void SetPrefabPath(fq::game_module::PrefabObject::Path path) { mPrefabPath = path; }
+
+	private:
+		Path mPrefabPath;
+	};
+}
