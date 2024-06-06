d46043c94b373c00e8a2d6b72e25caf138b32b92
DESKTOP-70GEFL6\user <17637yu@gmail.com>
2024-06-06 11:19:27 +0900

    Feat: 몬스터가 공격할 위치에 Attack 생성하게 추가

--- code/FourQuest/FQClient/MonsterAttack.cpp
+++ code/FourQuest/FQClient/MonsterAttack.cpp
@@ -40,35 +40,32 @@ void fq::client::MonsterAttack::OnStateUpdate(fq::game_module::Animator& animato
 		auto collider = object->GetComponent<fq::game_module::BoxCollider>();
 		auto attack = object->GetComponent<fq::client::Attack>();
 
+		// 공격 위한 세팅 
 		attack->SetAttackPower(20);
 		collider->SetExtent({ 0.4f, 0.2f, 0.2f });
+		object->SetTag(fq::game_module::ETag::Alien);
 		
-		DirectX::SimpleMath::Vector3 createVector = { 0, 1.25f, 0.5f };
+		/// 위치 설정 
+		// 몬스터 회전 값 가져와서 회전 
+		DirectX::SimpleMath::Quaternion monsterRoatation = monsterTransform->GetWorldRotation();
+		object->GetComponent<fq::game_module::Transform>()->SetLocalRotation({
+			monsterRoatation });
 
-		// 플레이어 위치
-		DirectX::SimpleMath::Vector3 playerPosition = monster->GetTarget()->GetComponent<fq::game_module::Transform>()->GetWorldPosition();
-		// 내위치
-		auto monsterTransform = monster->GetComponent<fq::game_module::Transform>();
-		DirectX::SimpleMath::Vector3 monsterPostion = monsterTransform->GetWorldPosition();
-		// 빼기
-		DirectX::SimpleMath::Vector3 directionVector = monsterPostion - playerPosition;
-		directionVector.Normalize();
+		// 얘도 Z 가 원래 0.5f 가 맞는데 기가막히게 뒤로 생긴다. ???
+		DirectX::SimpleMath::Vector3 createVector = { 0, 1.25f, -0.5f }; // 콜라이더 생성 위치 벡터
 
-		createVector = {createVector.x * directionVector.x + 
-		createVector.y * directionVector.y,
-		createVector.z * directionVector.z };
+		DirectX::SimpleMath::Matrix rotationMatrix = DirectX::SimpleMath::Matrix::CreateFromQuaternion(monsterRoatation);
+		createVector = DirectX::SimpleMath::Vector3::Transform(createVector, rotationMatrix);
 
 		object->GetComponent<fq::game_module::Transform>()->SetLocalPosition({ 
 			monsterTransform->GetWorldPosition().x + createVector.x,
 			monsterTransform->GetWorldPosition().y + createVector.y,
 			monsterTransform->GetWorldPosition().z + createVector.z });
 
-		object->GetComponent<fq::game_module::Transform>()->SetLocalRotation({
-			monsterTransform->GetWorldRotation()});
-		object->SetTag(fq::game_module::ETag::Alien);
-
+		// 생성
 		monster->GetScene()->AddGameObject(object);
 
+		// 공격 이후 설정 
 		monster->SetTarget(nullptr);
 		animator.SetParameterTrigger("OnIdle");
 
