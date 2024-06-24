c149025367d920a5930e4e012c4e218298d7a99c
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2024-06-24 19:21:41 +0900

    feat : height 맵 쿠킹

--- code/FourQuest/FQphysics/PhysicsCookingMeshTool.cpp
+++ code/FourQuest/FQphysics/PhysicsCookingMeshTool.cpp
@@ -29,4 +29,35 @@ namespace fq::physics
 
 		return convexMesh;
 	}
+
+
+	physx::PxTriangleMesh* PhysicsCookingMeshTool::CookingTriangleMesh(physx::PxPhysics* physics, DirectX::SimpleMath::Vector3* vertices, unsigned int vertexSize, unsigned int* indices, unsigned int indexSize)
+	{
+		physx::PxTolerancesScale scale;
+		physx::PxCookingParams params(scale);
+		// 메쉬 청소 사용 안 함 - 개발 구성에 대해 메쉬 유효성 검사 수행
+		params.meshPreprocessParams |= physx::PxMeshPreprocessingFlag::eDISABLE_CLEAN_MESH;
+		// 에지 사전 계산 사용 안 함, 각 삼각형에 대해 에지 설정, 연락처 생성 속도 저하
+		params.meshPreprocessParams |= physx::PxMeshPreprocessingFlag::eDISABLE_ACTIVE_EDGES_PRECOMPUTE;
+		// lower hierarchy for internal mesh
+		//params.meshCookingHint = PxMeshCookingHint::eCOOKING_PERFORMANCE;
+
+		physx::PxTriangleMeshDesc meshDesc;
+		meshDesc.points.count = vertexSize;
+		meshDesc.points.stride = sizeof(physx::PxVec3);
+		meshDesc.points.data = vertices;
+
+		meshDesc.triangles.count = indexSize;
+		meshDesc.triangles.stride = 3 * sizeof(unsigned int);
+		meshDesc.triangles.data = indices;
+
+#ifdef _DEBUG
+		// 메쉬 청소를 하지 않고 조리하기 전에 메쉬의 유효성을 확인해야 합니다
+		bool res = PxValidateTriangleMesh(params, meshDesc);
+		PX_ASSERT(res);
+#endif
+
+		physx::PxTriangleMesh* aTriangleMesh = PxCreateTriangleMesh(params, meshDesc,
+			physics->getPhysicsInsertionCallback());
+	}
 }
