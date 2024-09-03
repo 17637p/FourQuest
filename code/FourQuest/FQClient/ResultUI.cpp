615f8065d3b8f0649ae429ea4dc51e41c5b6e45a
YuJin <17637yu@gmail.com>
2024-09-03 13:14:14 +0900

    Feat: UI 사각형 그리기, 퀘스트 클리어 포탈 생성 기능 추가

--- code/FourQuest/FQClient/ResultUI.cpp
+++ code/FourQuest/FQClient/ResultUI.cpp
@@ -1,1 +1,57 @@
 #include "ResultUI.h"
+
+#include "../FQGameModule/Transform.h"
+#include "../FQGameModule/Scene.h"
+#include "../FQGameModule/ScreenManager.h"
+
+fq::client::ResultUI::ResultUI()
+	:mScreenManager(nullptr)
+{
+
+}
+
+fq::client::ResultUI::~ResultUI()
+{
+
+}
+
+void fq::client::ResultUI::OnStart()
+{
+	mScreenManager = GetScene()->GetScreenManager();
+}
+
+void fq::client::ResultUI::OnUpdate(float dt)
+{
+
+}
+
+std::shared_ptr<fq::game_module::Component> fq::client::ResultUI::Clone(std::shared_ptr<Component> clone /* = nullptr */) const
+{
+	auto resultUI = std::dynamic_pointer_cast<ResultUI>(clone);
+
+	if (resultUI == nullptr) // 새로 생성해서 복사본을 준다
+	{
+		resultUI = game_module::ObjectPool::GetInstance()->Assign<ResultUI>(*this);
+	}
+	else // clone에 데이터를 복사한다.
+	{
+		// 기본 대입 연산자 호출한다.
+		*resultUI = *this;
+	}
+
+	return resultUI;
+}
+
+void fq::client::ResultUI::SetScaleScreen()
+{
+	game_module::Transform* myTransform = GetComponent<game_module::Transform>();
+
+	// Scale 자동 조정 
+	UINT screenWidth = mScreenManager->GetFixScreenWidth();
+	UINT screenHeight = mScreenManager->GetFixScreenHeight();
+	float scaleX = screenWidth / (float)1920;
+	float scaleY = screenHeight / (float)1080;
+	{
+		myTransform->SetLocalScale({ scaleX, scaleY , 1 });
+	}
+}
--- code/FourQuest/FQClient/ResultUI.h
+++ code/FourQuest/FQClient/ResultUI.h
@@ -1,11 +1,33 @@
 #pragma once
 #include "../FQGameModule/Component.h"
 
+namespace fq::game_module
+{
+	class ScreenManager;
+}
+
 namespace fq::client
 {
 	class ResultUI : public game_module::Component
 	{
+	public:
+		ResultUI();
+		~ResultUI();
 
+		virtual void OnStart() override;
+		virtual void OnUpdate(float dt) override;
+
+	private:
+		entt::meta_handle GetHandle() override { return *this; }
+		std::shared_ptr<Component> Clone(std::shared_ptr<Component> clone /* = nullptr */)const override;
+
+		void SetScaleScreen();
+
+	private:
+		game_module::ScreenManager* mScreenManager;
+
+	private:
+		friend void RegisterMetaData();
 	};
 }
 
