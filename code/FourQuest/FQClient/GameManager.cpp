cfac0d15cd5ce3887c705ac32aad98cdbfca4352
DESKTOP-R3NEADA\user <17637yu@gmail.com>
2024-09-11 13:40:18 +0900

    Test: 게임 매니저 씬 전환시 플레이어 소환 추가 (알파용)

--- code/FourQuest/FQClient/GameManager.cpp
+++ code/FourQuest/FQClient/GameManager.cpp
@@ -1,8 +1,13 @@
 #include "GameManager.h"
 
-#include "../FQGameModule/GameModule.h"
 #include "ClientEvent.h"
 #include "Player.h"
+
+#include "../FQGameModule/GameModule.h"
+#include "../FQGameModule/CharacterController.h"
+
+// temp
+#include "PlayerInfoVariable.h"
 
 std::shared_ptr<fq::game_module::Component> fq::client::GameManager::Clone(std::shared_ptr<Component> clone /* = nullptr */) const
 {
@@ -27,7 +32,8 @@ fq::client::GameManager::~GameManager()
 }
 
 fq::client::GameManager::GameManager()
-	:mIsStop(false)
+	:mIsStop(false),
+	mIsAutoSpawn(false)
 	//	:mRegisterPlayerHandler{}
 {}
 
@@ -36,6 +42,12 @@ fq::client::GameManager::GameManager(const GameManager& other)
 	, mPlayers(other.mPlayers)
 	, mPauseUI(other.mPauseUI)
 	, mIsStop(false)
+	, mIsAutoSpawn(other.mIsAutoSpawn)
+	, mSoul(other.mSoul)
+	, mKnight(other.mKnight)
+	, mMagic(other.mMagic)
+	, mArcher(other.mArcher)
+	, mWarrior(other.mWarrior)
 {}
 
 fq::client::GameManager& fq::client::GameManager::operator=(const GameManager& other)
@@ -43,6 +55,12 @@ fq::client::GameManager& fq::client::GameManager::operator=(const GameManager& o
 	mPlayers = other.mPlayers;
 	mPauseUI = other.mPauseUI;
 	mIsStop = false;
+	mIsAutoSpawn = other.mIsAutoSpawn;
+	mSoul = other.mSoul;
+	mKnight = other.mKnight;
+	mMagic = other.mMagic;
+	mArcher = other.mArcher;
+	mWarrior = other.mWarrior;
 
 	return *this;
 }
@@ -84,6 +102,94 @@ void fq::client::GameManager::OnStart()
 {
 	EventProcessOffPopupPause();
 	EventProcessOffPopupSetting();
+
+	if (mIsAutoSpawn)
+	{
+		int playerID = 0;
+		switch (PlayerInfoVariable::Player1State)
+		{
+			case 0:
+				SpawnPlayer(mSoul, playerID);
+				break;
+			case 1:
+				SpawnPlayer(mKnight, playerID);
+				break;
+			case 2:
+				SpawnPlayer(mMagic, playerID);
+				break;
+			case 3:
+				SpawnPlayer(mArcher, playerID);
+				break;
+			case 4:
+				SpawnPlayer(mWarrior, playerID);
+				break;
+			default:
+				break;
+		}
+		playerID = 1;
+		switch (PlayerInfoVariable::Player2State)
+		{
+			case 0:
+				SpawnPlayer(mSoul, playerID);
+				break;
+			case 1:
+				SpawnPlayer(mKnight, playerID);
+				break;
+			case 2:
+				SpawnPlayer(mMagic, playerID);
+				break;
+			case 3:
+				SpawnPlayer(mArcher, playerID);
+				break;
+			case 4:
+				SpawnPlayer(mWarrior, playerID);
+				break;
+			default:
+				break;
+		}
+		playerID = 2;
+		switch (PlayerInfoVariable::Player3State)
+		{
+			case 0:
+				SpawnPlayer(mSoul, playerID);
+				break;
+			case 1:
+				SpawnPlayer(mKnight, playerID);
+				break;
+			case 2:
+				SpawnPlayer(mMagic, playerID);
+				break;
+			case 3:
+				SpawnPlayer(mArcher, playerID);
+				break;
+			case 4:
+				SpawnPlayer(mWarrior, playerID);
+				break;
+			default:
+				break;
+		}
+		playerID = 3;
+		switch (PlayerInfoVariable::Player4State)
+		{
+			case 0:
+				SpawnPlayer(mSoul, playerID);
+				break;
+			case 1:
+				SpawnPlayer(mKnight, playerID);
+				break;
+			case 2:
+				SpawnPlayer(mMagic, playerID);
+				break;
+			case 3:
+				SpawnPlayer(mArcher, playerID);
+				break;
+			case 4:
+				SpawnPlayer(mWarrior, playerID);
+				break;
+			default:
+				break;
+		}
+	}
 }
 
 void fq::client::GameManager::OnAwake()
@@ -132,23 +238,151 @@ void fq::client::GameManager::testKey()
 		for (auto& player : mPlayers)
 		{
 			auto playerComponent = player->GetComponent<Player>();
-			playerComponent->SetHp(100000.f);
+			if (playerComponent != nullptr)
+			{
+				playerComponent->SetHp(100000.f);
+			}
 		}
 	}
 
 	if (input->IsKeyState(EKey::F2, EKeyState::Tap))
 	{
+		SavePlayerState();
 		GetScene()->GetEventManager()->FireEvent<fq::event::RequestChangeScene>({ "Scene2", true});
 	}
 	if (input->IsKeyState(EKey::F3, EKeyState::Tap))
 	{
+		SavePlayerState();
 		GetScene()->GetEventManager()->FireEvent<fq::event::RequestChangeScene>({ "Scene3", true });
 	}
 	if (input->IsKeyState(EKey::F4, EKeyState::Tap))
 	{
+		SavePlayerState();
 		GetScene()->GetEventManager()->FireEvent<fq::event::RequestChangeScene>({ "Scene4", true });	
 	}
 }
 
+void fq::client::GameManager::SavePlayerState()
+{
+	for (auto& player : mPlayers)
+	{
+		auto playerComponent = player->GetComponent<Player>();
+		auto controller = player->GetComponent<game_module::CharacterController>();
+		if (controller->GetControllerID() == 0)
+		{
+			if (playerComponent == nullptr)
+			{
+				PlayerInfoVariable::Player1State = 0;
+			}
+			else
+			{
+				if (playerComponent->GetArmourType() == EArmourType::Knight)
+				{
+					PlayerInfoVariable::Player1State = 1;
+				}
+				if (playerComponent->GetArmourType() == EArmourType::Magic)
+				{
+					PlayerInfoVariable::Player1State = 2;
+				}
+				if (playerComponent->GetArmourType() == EArmourType::Archer)
+				{
+					PlayerInfoVariable::Player1State = 3;
+				}
+				if (playerComponent->GetArmourType() == EArmourType::Warrior)
+				{
+					PlayerInfoVariable::Player1State = 4;
+				}
+			}
+		}
+		if (controller->GetControllerID() == 1)
+		{
+			if (playerComponent == nullptr)
+			{
+				PlayerInfoVariable::Player2State = 0;
+			}
+			else
+			{
+				if (playerComponent->GetArmourType() == EArmourType::Knight)
+				{
+					PlayerInfoVariable::Player2State = 1;
+				}
+				if (playerComponent->GetArmourType() == EArmourType::Magic)
+				{
+					PlayerInfoVariable::Player2State = 2;
+				}
+				if (playerComponent->GetArmourType() == EArmourType::Archer)
+				{
+					PlayerInfoVariable::Player2State = 3;
+				}
+				if (playerComponent->GetArmourType() == EArmourType::Warrior)
+				{
+					PlayerInfoVariable::Player2State = 4;
+				}
+			}
+		}
+		if (controller->GetControllerID() == 2)
+		{
+			if (playerComponent == nullptr)
+			{
+				PlayerInfoVariable::Player3State = 0;
+			}
+			else
+			{
+				if (playerComponent->GetArmourType() == EArmourType::Knight)
+				{
+					PlayerInfoVariable::Player3State = 1;
+				}
+				if (playerComponent->GetArmourType() == EArmourType::Magic)
+				{
+					PlayerInfoVariable::Player3State = 2;
+				}
+				if (playerComponent->GetArmourType() == EArmourType::Archer)
+				{
+					PlayerInfoVariable::Player3State = 3;
+				}
+				if (playerComponent->GetArmourType() == EArmourType::Warrior)
+				{
+					PlayerInfoVariable::Player3State = 4;
+				}
+			}
+		}
+		if (controller->GetControllerID() == 3)
+		{
+			if (playerComponent == nullptr)
+			{
+				PlayerInfoVariable::Player4State = 0;
+			}
+			else
+			{
+				if (playerComponent->GetArmourType() == EArmourType::Knight)
+				{
+					PlayerInfoVariable::Player4State = 1;
+				}
+				if (playerComponent->GetArmourType() == EArmourType::Magic)
+				{
+					PlayerInfoVariable::Player4State = 2;
+				}
+				if (playerComponent->GetArmourType() == EArmourType::Archer)
+				{
+					PlayerInfoVariable::Player4State = 3;
+				}
+				if (playerComponent->GetArmourType() == EArmourType::Warrior)
+				{
+					PlayerInfoVariable::Player4State = 4;
+				}
+			}
+		}
+	}
+}
 
+void fq::client::GameManager::SpawnPlayer(fq::game_module::PrefabResource prefab, int index)
+{
+	std::shared_ptr<game_module::GameObject> newObject;
+
+	auto instance = GetScene()->GetPrefabManager()->InstantiatePrefabResoure(prefab);
+	newObject = *(instance.begin());
+
+	GetScene()->AddGameObject(newObject);
+	newObject->GetComponent<game_module::CharacterController>()->SetControllerID(index);
+}
 
--- code/FourQuest/FQClient/PlayerInfoVariable.h
+++ code/FourQuest/FQClient/PlayerInfoVariable.h
@@ -17,10 +17,10 @@ namespace fq::client
 		static inline int Player4SoulType = -1;
 
 		// 플레이어 현재 상태 
-		static inline int Player1State = 0; // 0 영혼, 1 검방, 2 마법, 3 궁수, 4 광전사
-		static inline int Player2State = 0; 
-		static inline int Player3State = 0; 
-		static inline int Player4State = 0; 
+		static inline int Player1State = -1; // 0 영혼, 1 검방, 2 마법, 3 궁수, 4 광전사
+		static inline int Player2State = -1; 
+		static inline int Player3State = -1; 
+		static inline int Player4State = -1; 
 
 		// 플레이어 남은 HP
 		static inline float Player1HP = 100.f;
