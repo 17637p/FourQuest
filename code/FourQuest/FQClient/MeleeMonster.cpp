110397d6c8d169d8cefb323071d48d7a5c6edc8a
이기태 <107969311+gigiTae@users.noreply.github.com>
2024-09-12 15:13:17 +0900

    Fix: AimAissist 로직 수정

--- code/FourQuest/FQClient/MeleeMonster.cpp
+++ code/FourQuest/FQClient/MeleeMonster.cpp
@@ -375,8 +375,6 @@ void fq::client::MeleeMonster::LookAtTarget()
 		directionQuaternion = DirectX::SimpleMath::Quaternion::LookRotation(directV, { 0.f,1.f,0.f });
 	directionQuaternion.Normalize();
 
-	DirectX::SimpleMath::Matrix rotationMatrix = DirectX::SimpleMath::Matrix::CreateFromQuaternion(directionQuaternion);
-
 	DirectX::SimpleMath::Quaternion result =
 		DirectX::SimpleMath::Quaternion::Slerp(currentRotation, directionQuaternion, RotationSpeed);
 
